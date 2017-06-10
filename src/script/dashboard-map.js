93b6e7b3c32b9c0a4465736d17cfaac2363659a0
Lennie132 <lennart.v@live.nl>
2017-06-10 15:24:02 +0200

    layer selector now works. added monitoring_wells API and layer

--- src/script/dashboard-map.js
+++ src/script/dashboard-map.js
@@ -33,7 +33,94 @@ $(document).ready(function () {
 
 });
 
-function setDataSource(data) {
+function setMonitoringWellsDataSource(data) {
+    map.addSource("monitoring_wells", {
+        type: "geojson",
+        data: data
+        //cluster: true,
+        //clusterMaxZoom: 14, // Max zoom to cluster points on
+        //clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+    });
+
+
+
+    // map.addLayer({
+    //     id: "clusters",
+    //     type: "circle",
+    //     source: "monitoring-wells",
+    //     filter: ["has", "point_count"],
+    //     paint: {
+    //         "circle-color": {
+    //             property: "point_count",
+    //             type: "interval",
+    //             stops: [
+    //                 [0, "#51bbd6"],
+    //                 [2, "#f1f075"],
+    //                 [5, "#f28cb1"]
+    //             ]
+    //         },
+    //         "circle-radius": {
+    //             property: "point_count",
+    //             type: "interval",
+    //             stops: [
+    //                 [0, 20],
+    //                 [2, 30],
+    //                 [5, 40]
+    //             ]
+    //         }
+    //     }
+    // });
+
+    // map.addLayer({
+    //     id: "cluster-count",
+    //     type: "symbol",
+    //     source: "monitoring-wells",
+    //     filter: ["has", "point_count"],
+    //     layout: {
+    //         "text-field": "{point_count_abbreviated}",
+    //         "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+    //         "text-size": 12
+    //     }
+    // });
+
+    map.addLayer({
+        id: "monitoring_wells",
+        type: "circle",
+        source: "monitoring_wells",
+        //filter: ["!has", "point_count"],
+        paint: {
+            "circle-color": "#da7c30",
+            "circle-radius": 4,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff"
+        }
+    });
+
+    // Create a popup, but don't add it to the map yet.
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+
+    map.on('mouseenter', 'monitoring_wells', function (e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(e.features[0].geometry.coordinates)
+            .setHTML(e.features[0].properties.description)
+            .addTo(map);
+    });
+
+    map.on('mouseleave', 'monitoring_wells', function () {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+}
+
+
+function setReportsDataSource(data) {
     map.addSource("reports", {
         type: "geojson",
         data: data,
@@ -43,7 +130,7 @@ function setDataSource(data) {
     });
 
     map.addLayer({
-        id: "clusters",
+        id: "reports",
         type: "circle",
         source: "reports",
         filter: ["has", "point_count"],
@@ -70,7 +157,7 @@ function setDataSource(data) {
     });
 
     map.addLayer({
-        id: "cluster-count",
+        id: "reports-count",
         type: "symbol",
         source: "reports",
         filter: ["has", "point_count"],
@@ -82,12 +169,12 @@ function setDataSource(data) {
     });
 
     map.addLayer({
-        id: "unclustered-point",
+        id: "reports-point",
         type: "circle",
         source: "reports",
         filter: ["!has", "point_count"],
         paint: {
-            "circle-color": "#11b4da",
+            "circle-color": "#396ab1",
             "circle-radius": 4,
             "circle-stroke-width": 1,
             "circle-stroke-color": "#fff"
--- src/script/dashboard.js
+++ src/script/dashboard.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
 
     map.on('load', function () {
         getReports(onReportsReady);
+
+        getMonitoringWells(onMonitoringWellsReady);
     });
 
 });
@@ -30,11 +32,49 @@ function onReportsReady() {
     });
     geojson.features = features;
 
-    setDataSource(geojson);
+    setReportsDataSource(geojson);
+}
+
+function onMonitoringWellsReady() {
+    var json = $.parseJSON(this.responseText);
+    var features = [];
+    var geojson = {
+        "type": "FeatureCollection",
+        "features": ""
+    };
+
+    json.items.forEach(function (item) {
+        features.push(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [item.longitude, item.latitude]
+                },
+                "properties": {
+                    "name": item.name,
+                    "description": item.straat + " " + item.huisnummer
+                }
+            }
+        );
+    });
+    geojson.features = features;
+
+    setMonitoringWellsDataSource(geojson);
 }
 
 function getReports(callback) {
-    var url = "http://localhost/ondiep/api/webservice.php";
+    var url = "http://localhost/ondiep/api/reports.php";
+    var request = new XMLHttpRequest();
+    request.addEventListener('load', callback);
+    request.open("GET", url, true);
+    request.setRequestHeader("HTTP-ACCEPT", "application/json");
+    request.send();
+}
+
+
+function getMonitoringWells(callback) {
+    var url = "http://localhost/ondiep/api/monitoring_wells.php?limit=3050";
     var request = new XMLHttpRequest();
     request.addEventListener('load', callback);
     request.open("GET", url, true);
